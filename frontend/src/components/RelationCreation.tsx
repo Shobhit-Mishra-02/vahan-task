57344ff3d4de04346e6b5585b74db949df62227e
shobhit <shobhitmishra2002@gmail.com>
2024-05-16 17:34:42 +0530

    feat: integrated table creation feature

--- frontend/src/components/RelationCreation.tsx
+++ frontend/src/components/RelationCreation.tsx
@@ -1,83 +1,141 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PopupContainer from "./PopupContainer";
 
+interface fieldInterface {
+  name: string;
+  title: string;
+  type:
+    | "numeric"
+    | "timestamp"
+    | "date"
+    | "time"
+    | "boolean"
+    | "varchar"
+    | "text";
+  isPrimary: boolean;
+}
+
 function RelationCreation() {
+  const [tableStructure, setTableStructure] = useState([] as fieldInterface[]);
+  const [tableName, setTableName] = useState("");
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    let data = Object.fromEntries(formData.entries()) as { [k: string]: any };
+
+    if (Object.keys(data).includes("isPrimary")) {
+      data["isPrimary"] = true;
+    } else {
+      data["isPrimary"] = false;
+    }
+
+    setTableStructure([...tableStructure, data as fieldInterface]);
+
+    e.currentTarget.reset();
+  };
+
+  const handleRemove = (
+    e: React.MouseEvent<HTMLTableDataCellElement, MouseEvent>
+  ) => {
+    const id = e.currentTarget.id;
+
+    const filteredData = tableStructure.filter((field) => field.name !== id);
+
+    setTableStructure(filteredData);
+  };
+
+  const handleTableCreate = async () => {
+    if (!tableName.length) {
+      alert("Enter the table name");
+      return;
+    }
+
+    if (!tableStructure.length) {
+      alert("Add at least one field");
+      return;
+    }
+
+    let primaryKeyCount = 0;
+
+    for (let i = 0; i < tableStructure.length; i++) {
+      const element = tableStructure[i];
+
+      if (element.isPrimary) {
+        primaryKeyCount++;
+      }
+    }
+
+    if (primaryKeyCount !== 1) {
+      alert("Only one primary key should be present.");
+      return;
+    }
+
+    console.log(tableName);
+    console.log(tableStructure);
+
+    const response = await fetch("http://localhost:5000/addTable", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ tableName, fields: tableStructure }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
   const [isOpen, setOpen] = useState(false);
 
   const toggle = () => {
     setOpen(!isOpen);
   };
 
+  useEffect(() => {
+    console.log(tableStructure);
+  }, [tableStructure]);
+
   return (
     <div>
       <PopupContainer isOpen={isOpen} toggle={toggle}>
         <div className="text-2xl pb-4">Add Field</div>
-        <form>
-          <label className="text-sm font-semibold text-gray-700" htmlFor="name">
+        <form onSubmit={(e) => handleFormSubmit(e)}>
+          <label className="label" htmlFor="name">
             Name
           </label>
-          <input
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2"
-            name="name"
-            type="text"
-          />
+          <input className="input" name="name" type="text" required />
 
-          <label
-            className="text-sm font-semibold text-gray-700"
-            htmlFor="title"
-          >
+          <label className="label" htmlFor="title">
             Title
           </label>
-          <input
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2"
-            name="title"
-            type="text"
-          />
+          <input className="input" name="title" type="text" required />
 
-          <label className="text-sm font-semibold text-gray-700" htmlFor="type">
+          <label className="label" htmlFor="type">
             Type
           </label>
-          <select
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2 bg-white"
-            name="type"
-            id="type"
-          >
+          <select className="input" name="type" id="type" required>
+            <option value="">select a type</option>
             <option value="varchar">varchar</option>
             <option value="numeric">numeric</option>
             <option value="timestamp">timestamp</option>
             <option value="date">date</option>
             <option value="time">time</option>
             <option value="text">text</option>
+            <option value="text">boolean</option>
           </select>
 
-          <label htmlFor="description">Description</label>
-          <textarea
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2 bg-white"
-            name="description"
-            id="description"
-            rows={3}
-          ></textarea>
+          <div className="flex justify-start gap-2 pt-6">
+            <input type="checkbox" name="isPrimary" />
+            <label className="label" htmlFor="isPrimary">
+              Primary key
+            </label>
+          </div>
 
-          <label htmlFor="timestamp">Timestamp</label>
-          <input
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2 bg-white"
-            type="datetime-local"
-          />
-
-          <label htmlFor="time">Time</label>
-          <input
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2 bg-white"
-            type="time"
-          />
-
-          <label htmlFor="date">Date</label>
-          <input
-            className="w-full p-1 text-xl text-gray-800 border rounded-sm focus:outline-black mb-2 bg-white"
-            type="date"
-          />
-
-          <div className="pt-4">
-            <button className="primaryBtn">Submit</button>
+          <div className="pt-8">
+            <button className="primaryBtn">Add</button>
           </div>
         </form>
       </PopupContainer>
@@ -87,6 +145,8 @@ function RelationCreation() {
           className="w-full py-2 text-3xl rounded-sm focus:outline-none"
           type="text"
           placeholder="Relation name"
+          value={tableName}
+          onChange={(e) => setTableName(e.target.value)}
         />
       </div>
 
@@ -121,36 +181,35 @@ function RelationCreation() {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b">
-              <td className="px-6 py-4">email</td>
-              <td className="px-6 py-4">Email</td>
-              <td className="px-6 py-4">varchar</td>
-              <td className="px-6 py-4">Primary key</td>
-              <td className="px-6 py-4 text-blue-500 underline font-semibold cursor-pointer hover:to-blue-600">
-                Edit
-              </td>
-              <td className="px-6 py-4 text-red-500 underline font-semibold cursor-pointer hover:to-red-600">
-                Remove
-              </td>
-            </tr>
-            <tr className="bg-white border-b">
-              <td className="px-6 py-4">email</td>
-              <td className="px-6 py-4">Email</td>
-              <td className="px-6 py-4">varchar</td>
-              <td className="px-6 py-4"></td>
-              <td className="px-6 py-4 text-blue-500 underline font-semibold cursor-pointer hover:to-blue-600">
-                Edit
-              </td>
-              <td className="px-6 py-4 text-red-500 underline font-semibold cursor-pointer hover:to-red-600">
-                Remove
-              </td>
-            </tr>
+            {tableStructure.map((field) => (
+              <tr key={field.name} className="bg-white border-b">
+                <td className="px-6 py-4">{field.name}</td>
+                <td className="px-6 py-4">{field.title}</td>
+                <td className="px-6 py-4">{field.type}</td>
+                <td className="px-6 py-4">
+                  {field.isPrimary ? "Primary Key" : ""}
+                </td>
+                {/* <td className="px-6 py-4 text-blue-500 underline font-semibold cursor-pointer hover:to-blue-600">
+                  Edit
+                </td> */}
+                <td
+                  id={field.name}
+                  className="px-6 py-4 text-red-500 underline font-semibold cursor-pointer hover:to-red-600"
+                  onClick={(e) => handleRemove(e)}
+                >
+                  Remove
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
 
       <div className="pt-8">
-        <button className="px-6 py-3 rounded-md bg-black text-white font-semibold">
+        <button
+          onClick={handleTableCreate}
+          className="px-6 py-3 rounded-md bg-black text-white font-semibold"
+        >
           Create
         </button>
       </div>
